dd3d3dddb99dabf1c9f425aef70c6fab50216d8d
Eliot Valdes <eliottvaldes@hotmail.com>
2023-06-20 12:37:14 -0600

    PP: docs: comentarios más descriptivos

--- 03_distance.py
+++ 03_distance.py
@@ -17,13 +17,18 @@ P3=(0.5,0.3,0.9)
 • Preferentemente emplea la distancia Euclidiana.
 • Deberás probar con al menos 20 puntos los cuales podrán generarse de manera aleatoria,
 """
-
+# random para generar números aleatorios, 
 import random
+
+# math para realizar cálculos matemáticos 
 import math
+
+# combinations de itertools para generar combinaciones de puntos.
 from itertools import combinations
 
 # Definir una clase para implementar un punto en 3D
 class Point3D:
+    # definimos el constructor que inicia las coordenadas del punto
     def __init__(self, x, y, z):
         """
         Inicializa un punto en 3D con las coordenadas (x, y, z).
@@ -32,12 +37,14 @@ class Point3D:
         self.y = y
         self.z = z
 
+    # definimos el método para calcular la distancia entre dos puntos. usamos distancia euclidiana
     def distance_to(self, other_point):
         """
         Calcula y devuelve la distancia Euclidiana a otro punto.
         """
         return math.sqrt((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2 + (self.z - other_point.z) ** 2)
 
+    # definimos el método para imprimir el punto como una cadena de texto
     def __str__(self):
         """
         Convierte el punto a una cadena para facilitar la impresión.
@@ -49,15 +56,21 @@ def closest_pair(points):
     """
     Encuentra y devuelve el par de puntos más cercano y su distancia.
     """
+    # Inicializar la distancia mínima con infinito
     min_distance = float('inf')
+    # Inicializar el par de puntos más cercano con None
     closest_pair = None
 
+    # Iterar sobre todas las combinaciones de puntos
     for p1, p2 in combinations(points, 2):
+        # obtenemos la distancia entre los puntos
         distance = p1.distance_to(p2)
+        # verificar si la distancia es menor a la distancia mínima
         if distance < min_distance:
             min_distance = distance
+            # si la distancia es menor, actualizamos el par de puntos más cercano
             closest_pair = (p1, p2)
-
+    # retornamos el par de puntos más cercano y su distancia
     return closest_pair, min_distance
 
 
@@ -74,4 +87,5 @@ print("\n==========================================================")
 # Encontrar el par de puntos más cercano
 pair, distance = closest_pair(points)
 
-print(f"\nEl par de puntos mas cercano es:\n{pair[0]} y {pair[1]}. \nCon una distancia de {distance}")
+print(f"\nEl par de puntos mas cercano es:\nPunto A: {pair[0]} \nPunto B: {pair[1]}. ")
+print(f"La distancia entre ellos es: {distance}.\n")
